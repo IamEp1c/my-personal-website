d16002cf0d6d5042cc101ec3328ade57dcae78cb
Waqas Anjum <Qanjum96@gmail.com>
2022-03-03 00:43:33 -0500

    added a lot more html and css to fix layout

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const h2 = document.createElement('h2');
-h2.textContent = 'This content added by JavaScript';
-document.querySelector('body').appendChild(h2);
+const h1 = document.createElement('h1')
+h1.textContent = "Biography"
+document.querySelector('body').appendChild(h1);
 
+
+
+
+const stateofNJ = document.getElementById('stateofnj');
+stateofNJ.onmouseover; 
