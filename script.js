d3f0ff3168db42e9d391d98c99c8e0b45d510382
Waqas Anjum <Qanjum96@gmail.com>
2022-03-05 00:45:57 -0500

    deleted some html, did some redesign

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const h1 = document.createElement('h1')
-h1.textContent = "Biography"
-document.querySelector('body').appendChild(h1);
+h1.textContent = "Welcome to my page!"
+
 
 
 
 
 const stateofNJ = document.getElementById('stateofnj');
 stateofNJ.onmouseover; 
+
+
+
